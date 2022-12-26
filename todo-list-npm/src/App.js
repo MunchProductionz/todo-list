68a8ba3f2b5e017571997d1d918df917e8c65d20
Henrik <henrrb@stud.ntnu.no>
2022-12-26 09:25:36 +0100

    Add useEffect so stored todos are shown

--- todo-list-npm/src/App.js
+++ todo-list-npm/src/App.js
@@ -9,6 +9,13 @@ function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
 
