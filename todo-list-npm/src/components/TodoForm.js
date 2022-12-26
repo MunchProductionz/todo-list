19f50db8a0f202f885fed78bb5df03e61862a364
Henrik <henrrb@stud.ntnu.no>
2022-12-26 08:47:51 +0100

    Add text to button

--- todo-list-npm/src/components/TodoForm.js
+++ todo-list-npm/src/components/TodoForm.js
@@ -31,7 +31,7 @@ function TodoForm({ addTodo }) {
                 value={todo.task}
                 onChange={handleTaskInputChange}
             />
-            <button />
+            <button type="submit">Submit</button>
         </form>
     );
     
